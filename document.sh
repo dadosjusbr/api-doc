0d8535070547b299d860ba270cbd94cc16487cda
Manuel Eduardo Nascimento Antunes <mena1@aluno.ifal.edu.br>
2021-09-28 14:33:28 -0300

    fix: improve the doc automatization

---
+++ document.sh
@@ -0,0 +1,5 @@
+npx insomnia-documenter --config ./dadosjusdoc.json --logo ./assets/logo.png
+cp assets/favicon.ico favicon.ico
+cp assets/default.html index.html
+cp insomnia.json assets/insomnia.json
+npx serve
